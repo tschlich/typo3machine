72cace362f82c94f4596c7c2c1f7b7982a916ad9
Thomas Schlich <t.schlich@mad-world.de>
2014-11-15 04:43:14 +0100

    Setup: Webroot erstellen und Apache testen

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -11,3 +11,13 @@ apt-get install -y apache2
 # Webroot auf den gemeinsamen Ordner umleiten
 rm -rf /var/www
 ln -fs /vagrant /var/www
+
+# Apache2 default webroot-Verzeichnis erstellen
+# @todo evtl. vhost erstellen
+mkdir /var/www/html
+cd /var/www/html
+
+# Testseite erstellen, aufrufen und löschen
+echo "Apache funktioniert!" > apacheTest.html
+wget -qO- 127.0.0.1/apacheTest.html
+rm apacheTest.html
