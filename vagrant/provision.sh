24e787316b26b302b72cf4002b7564e02685e0bb
Thomas Schlich <t.schlich@mad-world.de>
2014-11-24 23:52:38 +0100

    debug marker added

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -3,6 +3,8 @@
 # execution time start
 startTime=$(date +%s)
 
+# debug marker
+marker="#######################################################################"$'\r\n'
 ### !! IMPORTANT !! ############################################################
 # set development false to avoid data loss
 # also set development false on first vm start when developing
@@ -24,18 +26,18 @@ development=false
 flagInstalled="/home/vagrant/flagInstalled"
 if [ -f $flagInstalled ]
   then
-    echo "### system allready started"
-  else
-    echo "### starting project installation"
+    echo "${marker}### system allready started"
+  #else
+    echo "${marker}### starting project installation"
     # remove linebreak when not developing on vagrant>>
     touch $flagInstalled #<<
 
     if [ "$development" = false ] ; then
 
-      echo "### go to folder /var/www/components"
+      echo "${marker}### go to folder /var/www/components"
       cd /var/www/components
 
-      echo "### installing nodejs (apt sources will be updatet)"
+      echo "${marker}### installing nodejs (apt sources will be updated)"
       # adding PPA in order to get access to its contents
       curl -sL https://deb.nodesource.com/setup | sudo bash -
       sudo apt-get install -y nodejs
@@ -43,72 +45,73 @@ if [ -f $flagInstalled ]
       # The nodejs package contains the nodejs binary as well as npm
       # in order for some npm packages to work (such as those that require 
       # building from source), build-essentials package will be installed
-      echo "### installing build-essential"
+      echo "${marker}### installing build-essential"
       sudo apt-get install -y build-essential
 
-      echo "### installing bower global"
+      echo "${marker}### installing bower global"
       npm install -g bower
 
-      echo "### installing grunt global"
+      echo "${marker}### installing grunt global"
       npm install -g grunt-cli
 
-      echo "### installing nodejs packages"
+      echo "${marker}### installing nodejs packages"
       npm install
 
       echo "installing bower packages" 
       sudo -H -u vagrant bower install
 
-      #echo "### installing git"
+      #echo "${marker}### installing git"
       #apt-get install -y git
 
-      echo "### installing apache2"
+      echo "${marker}### installing apache2"
       apt-get install -y apache2
 
-      echo "### removing html dummy"
+      echo "${marker}### removing html dummy"
       rm /var/www/html/index.html
 
-      echo "### enabling vhost"
+      echo "${marker}### enabling vhost"
       a2ensite thosh-t3dist
       # @todo https://
 
-      echo "### adding user t3dist, groups: www-data"
+      echo "${marker}### adding user t3dist, groups: www-data"
       useradd -G www-data -m -s /bin/bash t3dist
 
-      echo "### setting password to t3dist"
+      echo "${marker}### setting password to t3dist"
       echo "t3dist:t3dist" | chpasswd
 
-      echo "### adding user dev, groups: vagrant, admin, www-data, t3dist"
+      echo "${marker}### adding user dev, groups: vagrant, admin, www-data, t3dist"
       useradd -G vagrant,admin,www-data,t3dist -m -s /bin/bash dev
 
-      echo "### setting password to dev"
+      echo "${marker}### setting password to dev"
       echo "dev:dev" | chpasswd
 
-      echo "### adding groups to existing user vagrant: www-data, dev, t3dist"
+      echo "${marker}### adding groups to existing user vagrant: www-data, dev, t3dist"
       usermod -a -G www-data,dev,t3dist vagrant
 
-      echo "### installing mysql using noninteractive mode"
+      echo "${marker}### installing mysql using noninteractive mode"
       export DEBIAN_FRONTEND=noninteractive 
       apt-get install -q -y mysql-server
 
-      echo "### setting password for mysql user root to root"
+      echo "${marker}### setting password for mysql user root to root"
       mysqladmin -u root password root
 
-      echo "### creating mysql user t3dist with password t3dist"
+      echo "${marker}### creating mysql user t3dist with password t3dist"
       mysql -uroot -proot -e "CREATE USER 't3dist'@'localhost' IDENTIFIED BY 't3dist'"
 
-      echo "### creating database t3dist"
+      echo "${marker}### creating database t3dist"
       mysql -uroot -proot -e "CREATE DATABASE t3dist CHARACTER SET utf8 COLLATE utf8_general_ci;"
 
-      echo "### setting t3dist privileges"
+      echo "${marker}### setting t3dist privileges"
       # @todo check privileges 
       mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON t3dist . * TO 't3dist'@'localhost'"
 
-      echo "### flushing privileges"
+      echo "${marker}### flushing privileges"
       mysql -uroot -proot -e "FLUSH PRIVILEGES"
 
-      echo "### installing php"
+      echo "${marker}### installing php"
       apt-get install -y php5
       apt-get install -y php5-mysql
+      # @todo phpmyadmin reports error
       apt-get install -y php5-mcrypt
       #apt-get install -y libapache2-mod-auth-mysql
       #apt-get install -y php5-gd 
@@ -124,14 +127,14 @@ if [ -f $flagInstalled ]
        sudo sed -i "s/^\($key\).*/\1 $(eval echo = \${$key})/" /etc/php5/apache2/php.ini
       done
 
-      echo "### restarting apache"
+      echo "${marker}### restarting apache"
       apache2ctl restart
 
-      echo "### installing additional packages"
+      echo "${marker}### installing additional packages"
       apt-get install -y freetype* 
       apt-get install -y graphicsmagick
 
-      echo "### installing phpMyAdmin"
+      echo "${marker}### installing phpMyAdmin"
       # Configure the Settings with debconf-set-selections
       echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
       echo 'phpmyadmin phpmyadmin/app-password-confirm password root' | debconf-set-selections
@@ -141,25 +144,25 @@ if [ -f $flagInstalled ]
       # Install phpmyadmin with noninteractive mode (it will use the set settings)
       apt-get install -q -y phpmyadmin
 
-      echo "### installing typo3"
+      echo "${marker}### installing typo3"
       cd /var/www/components
       if [ -d typo3_src-6.2.6 ]
         then
-          echo "### sourcecode exists, no action"
+          echo "${marker}### sourcecode exists, no action"
           # @todo check md5
         else
           if [ -f 6.2 ]
             then
-              echo "### tarball exists, unpacking ..."
+              echo "${marker}### tarball exists, unpacking ..."
             else
-              echo "### loading tarball (current 6.2.x) ..."
+              echo "${marker}### loading tarball (current 6.2.x) ..."
               wget -q http://get.typo3.org/6.2
           fi # /-f 6.2
 
-          echo "### unpacking tarball ..."
+          echo "${marker}### unpacking tarball ..."
           tar -xzf 6.2
 
-          echo "### deleting tarball"
+          echo "${marker}### deleting tarball"
           rm 6.2
 
           echo "### creating symlinks"
@@ -204,4 +207,4 @@ fi # /flagInstalled
 finishTime=$(date +%s)
 executionTimeSec=$((finishTime - startTime))
 executionTime=$(date -u -d @${executionTimeSec} +"%T")
-echo "### Execution time: $executionTime hh:mm:ss"
+echo "${marker}### Execution time: $executionTime hh:mm:ss"
