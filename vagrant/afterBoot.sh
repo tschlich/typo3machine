888384eac0b7f63452a2c80f7fcf7e819e6f9bff
Thomas Schlich <t.schlich@mad-world.de>
2014-11-24 21:54:18 +0100

    Apache2 workaround, trying to start apache before synced folders are mounted

--- vagrant/afterBoot.sh
+++ vagrant/afterBoot.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-### Dieses Script wird nach jedem Neustart der VM ausgeführt ###
-echo "### afterBoot.sh wird ausgeführt"
+###########################################################
+################ Apache2 Workaround #######################
+###########################################################
+
+# Apache2 is getting Started before Shared Folders are Mounted by Vagrant
+# Simply starts the Apache2 after the init Booting is done.
+# Runs on every Boot
+apache2ctl start
+
